d7bf4153ae1ddbe01d9ba3d22026e9b69e6f50d0
Akib Us Suny Eshan <eshan.r029@gmail.com>
2024-10-24 15:10:12 +0600

    updated

--- practice/encapsulation.py
+++ practice/encapsulation.py
@@ -1,7 +1,7 @@
 class Parent:
     def __init__(self,name,fathername):
-        self.name=name
+        self.__name=name
         self.fathername=fathername
         
 obj1=Parent("Eshan","Abu Taher")        
-print(obj1.fathername)    
+print(obj1.__name)    
