70cfb1c03d46316677155ef78634738448054777
Akib Us Suny Eshan <I-484@wgbd.com>
2024-10-29 12:14:45 +0600

    updated

--- practice/polymorphism.py
+++ practice/polymorphism.py
@@ -58,3 +58,35 @@ for person in (boy1,girl):
   person.gender()
   person.info() 
   person.gender()
+ 
+#Example3 
+
+from math import pi  
+
+class Shape:
+    def __init__(self, name):
+        self.name = name
+
+class Rectangle(Shape):
+    def __init__(self, name, length, width):
+        self.length = length
+        self.width = width
+        super().__init__(name)
+        
+    def area(self):
+        return self.length * self.width
+        
+class Circle(Shape):
+    def __init__(self, name, radius):
+        self.radius = radius
+        super().__init__(name)
+      
+    def area(self):
+        return pi * self.radius * self.radius
+
+                
+rectangle1 = Rectangle("Rec", 5, 3)  
+circle1 = Circle("Cir", 5)        
+
+print(f"Here is the output: The area of {rectangle1.name} is {rectangle1.area()}")
+print(f"The area of the {circle1.name} is: {circle1.area()}")
