ea7863f81ecb97f9d147d8d098bc08c215d6d430
Akib Us Suny Eshan <I-484@wgbd.com>
2024-10-31 10:28:26 +0600

    updated

---
+++ main/main.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    dbname="postgress",  
+    user="postgres",
+    password="1234",  
+)
+
+cur = conn.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS person (r
+        id INT PRIMARY KEY,
+        name VARCHAR(255),
+        age INT,
+        gender CHAR
+    );
+""")
+
+conn.commit()
+cur.close()
+conn.close()
